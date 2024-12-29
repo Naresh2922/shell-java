a5606f82b9a9ec4c4f8e7760a04d8a105c439cff
Nb2922 <nareshsabbavarapu05@gmail.com>
2024-12-29 12:39:42 +0530

    repl-with-exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run(){
+                System.out.println("JVM is shutting down.");
+            }
+        });
         
         try(Scanner scanner = new Scanner(System.in)){
             while(true){
                 System.out.print("$ ");
                 String input = scanner.nextLine();
+                String[] inputArray = input.split(" ");
+                if(inputArray[0].equals("exit") && inputArray.length == 2){
+                    try{
+                        if(Integer.parseInt(inputArray[1]) == 0){
+                            System.exit(0);
+                        }
+                    } catch (NumberFormatException nfe){
+                        System.err.println("format -- exit <integer> :" + nfe.getMessage());
+                    }
+                    
+                }
                 System.err.println(input + ": " + "command not found");
             }         
         }       
